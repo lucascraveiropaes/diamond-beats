7d37be107158e01f4f75242c4c17d9b5f482023c
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2018-01-18 15:42:20 -0200

    Atualizações na homepage

--- platforms/browser/www/telas/js/search.js
+++ platforms/browser/www/telas/js/search.js
@@ -1,48 +1,67 @@
-function clearString(string) {
+function getGradient(prevGradient) {
+	var g = [
+	    "primary-gradient",
+	    "red-gradient",
+	    "pink-gradient",
+	    "baby-gradient",
+	    "blue-gradient",
+	    "blue-darker-gradient"
+	];
+
+	var i = Math.floor((Math.random() * g.length) + 1);
+
+	if (g[i] == undefined) {
+		g[i] = "primary-gradient";
+	}
+
+	//alert(g[i] + " - " + prevGradient);
+
+	if (g[i] == prevGradient) {
+    	g[i] = getGradient(prevGradient);
+    }
+
+	return g[i];
+}
+
+function clearString(string, search) {
     string = string.replace(/\(.*?\)/g, '');  // Remove () and inside content
     string = string.replace(/\{.*?\}/g, '');  // Remove {} and inside content
     string = string.replace(/\[.*?\]/g, '');  // Remove [] and inside content
     string = string.replace('VEVO', '');
 
+    searchSize = search.length;
+    test = string.substr(0,searchSize);
+
+    if( test.toLowerCase() == search.toLowerCase() ){
+        string = string.(searchSize, string.length);
+    }
+
     return string;
 }
 
 function shortString(string) {
-    string = string.substr(0, 35);
+    string = string.substr(0, 20);
+    string = string.slice(0,-1);
     string += "...";
+
     return string;
 }
 
 function getRequestFromSearch(searchTerm) {
     url = 'https://www.googleapis.com/youtube/v3/search';
+
     var params = {
         maxResults: 12,
+        type: 'video',
         order: 'relevance',
         part: 'snippet',
         key: 'AIzaSyDAKDaBy_JDwcScSHqDQimOOLjdPImLanc',
         q: searchTerm
     };
-  
-    $.getJSON(url, params, function (searchTerm) {
-        showResults(searchTerm);
+
+    $.getJSON(url, params, function (data) {
+        showResultsFromSearch(data);
     });
-}
-
-function getRequest(params, type, placeIt = null) {
-    url = 'https://www.googleapis.com/youtube/v3/search';
-
-    if (type == "s") {
-        $.getJSON(url, params, function (searchTerm) {
-            showResultsFromSearch(searchTerm);
-        });
-    }
-    else if (type == "t") {
-        $.getJSON(url, params, function (searchTerm) {
-            showResults(searchTerm);
-        });
-    }
-  
-        
 }
 
 function showResultsFromSearch(results) {
@@ -66,20 +85,36 @@ function showResultsFromSearch(results) {
     $('#search-results').html(html);
 }
 
+function getRequest(params) {
+    url = 'https://www.googleapis.com/youtube/v3/search';
+
+    $.getJSON(url, params, function (searchTerm) {
+        showResults(searchTerm);
+    }); 
+        
+}
+
 function showResults(results, placeIt) {
     var html = "";
     var entries = results.items;
+	var prevGradient = null;
     
     $.each(entries, function (index, value) {
+
         var title = shortString(clearString(value.snippet.title));
         var thumbnail = value.snippet.thumbnails.default.url;
         var channel = clearString(value.snippet.channelTitle);
+
+        var gradient = getGradient(prevGradient);
 
         html += '<div class="col s12"><div class="card-panel transparent">';
         html += '<img src="' + thumbnail + '" class="thumbnail">';
         html += '<div class="content"><span class="title">'+ title + '</a></span>';
         html += '<span class="subtitle">by <a href="#">' + channel + '</a></span>';
-        html += '</div></div></div><hr class="simple-line-85">';
+        html += '</div></div></div>';
+        html += '<hr class="simple-line-85">';
+
+        prevGradient = gradient;
     });
 
     $(placeIt).html(html);
@@ -97,7 +132,11 @@ function getTrends() {
         key: 'AIzaSyDAKDaBy_JDwcScSHqDQimOOLjdPImLanc'
     };
 
-    getRequest(params, "#alta");
+    url = 'https://www.googleapis.com/youtube/v3/search';
+
+    $.getJSON(url, params, function (searchTerm) {
+        showResults(searchTerm, "#alta");
+    });
 }
 
 $(document).ready(function () {
@@ -105,6 +144,9 @@ $(document).ready(function () {
         event.preventDefault();
 
         var searchTerm = $('#search-term').val();
+
         getRequestFromSearch(searchTerm);
     });
+
+    getTrends();
 });
--- www/telas/js/search.js
+++ www/telas/js/search.js
@@ -1,15 +1,49 @@
-function clearString(string) {
+function getGradient(prevGradient) {
+	var g = [
+	    "primary-gradient",
+	    "red-gradient",
+	    "pink-gradient",
+	    "baby-gradient",
+	    "blue-gradient",
+	    "blue-darker-gradient"
+	];
+
+	var i = Math.floor((Math.random() * g.length) + 1);
+
+	if (g[i] == undefined) {
+		g[i] = "primary-gradient";
+	}
+
+	//alert(g[i] + " - " + prevGradient);
+
+	if (g[i] == prevGradient) {
+    	g[i] = getGradient(prevGradient);
+    }
+
+	return g[i];
+}
+
+function clearString(string, search) {
     string = string.replace(/\(.*?\)/g, '');  // Remove () and inside content
     string = string.replace(/\{.*?\}/g, '');  // Remove {} and inside content
     string = string.replace(/\[.*?\]/g, '');  // Remove [] and inside content
     string = string.replace('VEVO', '');
 
+    searchSize = search.length;
+    test = string.substr(0,searchSize);
+
+    /*if( test.toLowerCase() == search.toLowerCase() ){
+        string = string.(searchSize, string.length);
+    }*/
+
     return string;
 }
 
 function shortString(string) {
-    string = string.substr(0, 35);
+    string = string.substr(0, 20);
+    string = string.slice(0,-1);
     string += "...";
+
     return string;
 }
 
@@ -63,17 +97,24 @@ function getRequest(params) {
 function showResults(results, placeIt) {
     var html = "";
     var entries = results.items;
+	var prevGradient = null;
     
     $.each(entries, function (index, value) {
+
         var title = shortString(clearString(value.snippet.title));
         var thumbnail = value.snippet.thumbnails.default.url;
         var channel = clearString(value.snippet.channelTitle);
+
+        var gradient = getGradient(prevGradient);
 
         html += '<div class="col s12"><div class="card-panel transparent">';
         html += '<img src="' + thumbnail + '" class="thumbnail">';
         html += '<div class="content"><span class="title">'+ title + '</a></span>';
         html += '<span class="subtitle">by <a href="#">' + channel + '</a></span>';
-        html += '</div></div></div><hr class="simple-line-85">';
+        html += '</div></div></div>';
+        html += '<hr class="simple-line-85">';
+
+        prevGradient = gradient;
     });
 
     $(placeIt).html(html);
